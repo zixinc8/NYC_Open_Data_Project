009861a36d97052c63ef6b6cd0ab4586e7f58020
zixinc8 <zixinc8@nycstudents.net>
2026-05-28 18:08:17 +0000

    2

--- helper.js
+++ helper.js
@@ -2,20 +2,7 @@ function get(id){
   return document.getElementById(id);
 }
 
-function showMap(lat,lon){
-  let location = [lat, lon];
-  if(!mapObj){
-      mapObj = L.map("map");
-  } 
-  let map = mapObj.setView(location, 18);
-
-  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 18,
-    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
-  }).addTo(map);
-
-  let marker = L.marker(location).addTo(map);// places marker on map
-}
+let lat, lon;
 
 function card(info){ 
     let build = `<div class="card fitted">
@@ -28,9 +15,32 @@ function card(info){
                     <p>longitude: ${info.longitude}</p>
                     <hr>
                     <p>${info.restaurantinspectionid}</p>`;
-                     if(info.latitude && info.longitude){
-                       build += `<input type="button" value="Map" onclick="showMap( ${info.latitude}, ${info.longitude} )">`;
-                     }
+                    if(info.latitude && info.longitude){
+                      build += `<input type="button" value="Map" onclick="showMap( ${info.latitude}, ${info.longitude} )">`;
+                    }         
         build +=    `</div>`;
+        if(info.latitude && info.longitude){
+          lat = `${info.latitude}`;
+          lon = `${info.longitude}`;
+        }
      return build;
+}
+
+function showMap(lat,lon){
+  let location = [lat, lon];
+  if(!mapObj){
+      mapObj = L.map("map");
+  } 
+  let map = mapObj.setView(location, 18);
+
+  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+  }).addTo(map);
+
+  let marker = L.marker(location).addTo(map);
+}
+
+function displayMap(){
+  showMap(lat,lon);
 }
--- script.js
+++ script.js
@@ -29,12 +29,3 @@ function filterByBoro(){
   }
   leftPanel.innerHTML = build;
 }
-
-function displayMap(){
-  //Retrieve the latitude & longitude from the user via text inputs and pass it to the showMap() function to generate the map and display it.
-  let lat = get("lat").value;
-  let lon = get("lon").value;
-
-  showMap(lat,lon);
-  
-}
